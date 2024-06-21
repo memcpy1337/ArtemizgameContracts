6d877e8cb82541527ee41eb70aa820a5560ab3f5
VS\shabala <artemizgame@gmail.com>
2024-06-21 13:17:05 +1000

    2.0.2

---
+++ Events/MatchMakingEvents/MatchPlayerQueuedEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Events.MatchMakingEvents;
+
+public record MatchPlayerQueuedEvent
+{
+    public required string UserId { get; set; }
+}
