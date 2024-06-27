f24f448db46dae2ca386fccaa66edab084f6a331
VS\shabala <artemizgame@gmail.com>
2024-06-27 15:01:46 +1000

    Ref

--- Events/MatchMakingEvents/MatchPlayerQueuedEvent.cs
+++ Events/MatchMakingEvents/MatchPlayerQueuedEvent.cs
@@ -8,8 +8,5 @@ namespace Contracts.Events.MatchMakingEvents;
 
 public record MatchPlayerQueuedEvent
 {
-    public required string UserId { get; set; }
     public string Token { get; set; } = String.Empty;
-    public bool IsSuccess { get; set; }
-    public string Message { get; set; } = String.Empty;
 }
