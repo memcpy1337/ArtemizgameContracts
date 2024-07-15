730bbc7d65491ee693a2861ed874651233f156f5
VS\shabala <artemizgame@gmail.com>
2024-07-15 12:04:11 +1000

    Add data

---
+++ Common/Helpers/Helpers.cs
@@ -0,0 +1,55 @@
+﻿using Contracts.Common.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Common.Helpers;
+
+public static class Helpers
+{
+    public static int GetTargetPlayersCountForGameType(GameTypeEnum gameTypeEnum, PlayerTypeEnum playerType)
+    {
+        switch(gameTypeEnum)
+        {
+            case GameTypeEnum.Classic:
+                if (playerType == PlayerTypeEnum.Crew)
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 1;
+                }
+            case GameTypeEnum.BotMonster:
+                if (playerType == PlayerTypeEnum.Crew)
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 0;
+                }
+            case GameTypeEnum.OneByOne:
+                if (playerType == PlayerTypeEnum.Crew)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 1;
+                }
+            case GameTypeEnum.OneVsBot:
+                if (playerType == PlayerTypeEnum.Crew)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            default: return 0;
+        }
+    }
+}
---
+++ Common/Models/Enums/MatchStatusEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Common.Models.Enums;
+
+public enum MatchStatusEnum
+{
+    Init,
+    WaitForPlayers,
+    WaitForServer,
+    Process,
+    End
+}
