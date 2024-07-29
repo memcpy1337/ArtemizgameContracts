d87688a78820764ef931e86e0bfc1050a7e74b8e
VS\shabala <artemizgame@gmail.com>
2024-07-29 10:01:04 +1000

    Add

--- Events/MatchMakingEvents/MatchPlayerAddEvent.cs
+++ Events/MatchMakingEvents/MatchPlayerAddEvent.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Contracts.Common.Models.Enums;
 
 namespace Contracts.Events.MatchMakingEvents;
 
@@ -10,5 +6,5 @@ public record MatchPlayerAddEvent
 {
     public required string UserId { get; set; }
     public required string MatchId { get; set; }
-    public bool ServerWasReady { get; set; }
+    public MatchStatusEnum MatchStatus { get; set; }
 }
--- Events/ServerEvents/ServerPlayerConnectedEvent.cs
+++ Events/ServerEvents/ServerPlayerConnectedEvent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MassTransit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Contracts.Events.ServerEvents;
 
+[MessageUrn("user-connection-event")]
 public record ServerPlayerConnectedEvent
 {
     public required string ServerId { get; set; }
