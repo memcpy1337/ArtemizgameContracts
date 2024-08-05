be1583b8fc9c33404dd34c3331b1906d5b464bfc
VS\shabala <artemizgame@gmail.com>
2024-08-05 14:18:25 +1000

    Add event

---
+++ Events/ServerEvents/ServerConnectionDataUpdateEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Events.ServerEvents;
+
+public record ServerConnectionDataUpdateEvent
+{
+    public required string MatchId { get; set; }
+    public required string ServerId { get; set; }
+    public required string Address { get; set; }
+    public int Port { get; set; }
+}
