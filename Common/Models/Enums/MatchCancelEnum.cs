1c32bacbdafef5a9576e3424d9a7fe8422911b79
VS\shabala <artemizgame@gmail.com>
2024-08-07 09:27:29 +1000

    Add new enum

---
+++ Common/Models/Enums/MatchCancelEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Common.Models.Enums;
+
+public enum MatchCancelEnum
+{
+    NoPlayerInMatch,
+    SomePlayersNotConnected,
+    ServerCantBeInit,
+    InternalError
+}
