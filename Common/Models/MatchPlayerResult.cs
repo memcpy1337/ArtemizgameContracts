4a054d6fff0e5a8a73a1c9d0f073f6b6468264fc
VS\shabala <artemizgame@gmail.com>
2024-08-09 09:17:24 +1000

    New model

---
+++ Common/Models/MatchPlayerResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Common.Models;
+
+public class MatchPlayerResult
+{
+    public required string UserId { get; set; }
+    public int EloRatingBefore { get; set; }
+    public int EloRatingAfter { get; set; }
+}
