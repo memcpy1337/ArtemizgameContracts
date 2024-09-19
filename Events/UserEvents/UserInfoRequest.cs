8e4a6c059d595a12b4af955f23d3731382f72e32
VS\shabala <artemizgame@gmail.com>
2024-09-20 09:15:34 +1000

    New contracts

---
+++ Events/UserEvents/UserInfoRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Events.UserEvents;
+
+public record UserInfoRequest
+{
+    public required string UserId { get; init; }
+}
---
+++ Events/UserEvents/UserInfoResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Events.UserEvents;
+
+public record UserInfoResponse
+{
+    public required string UserId { get; set; }
+    public required string NickName { get; set; }
+}
